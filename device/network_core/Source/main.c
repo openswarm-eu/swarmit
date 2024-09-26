1e68502a62c52d2204371d7e0005b96a31a41a8e
Alexandre Abadie <alexandre.abadie@inria.fr>
2024-09-26 10:35:34 +0200

    device/network_core: remove CONSTLAT=1 use

--- device/network_core/Source/main.c
+++ device/network_core/Source/main.c
@@ -106,8 +106,6 @@ static void delay_ms(uint32_t ms) {
 int main(void) {
 
     _app_vars.device_id = _deviceid();
-    // Configure constant latency mode for better performances
-    NRF_POWER_NS->TASKS_CONSTLAT = 1;
 
     NRF_IPC_NS->INTENSET                            = (1 << IPC_CHAN_REQ) | (1 << IPC_CHAN_LOG_EVENT);
     NRF_IPC_NS->SEND_CNF[IPC_CHAN_EXPERIMENT_START] = 1 << IPC_CHAN_EXPERIMENT_START;
